aa762969de187ed5ccc427bd837d3a9baf1c3fb0
jasonkalmeida <jasonkalmeida@gmail.com>
2019-09-25 21:44:02 -0700

    Updates

---
+++ precache-manifest.ab4583b5a7636896ae9e2fc98034a876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c2ea0356953e7b59700d9001c999d4",
+    "url": "/myfoodapp/index.html"
+  },
+  {
+    "revision": "a419a8ecdbd9c2c986b5",
+    "url": "/myfoodapp/static/css/main.c030eec5.chunk.css"
+  },
+  {
+    "revision": "54dc0f89c26eca8c9942",
+    "url": "/myfoodapp/static/js/2.c38b69c9.chunk.js"
+  },
+  {
+    "revision": "a419a8ecdbd9c2c986b5",
+    "url": "/myfoodapp/static/js/main.3bcb6895.chunk.js"
+  },
+  {
+    "revision": "3b6ad565362481bfe2e6",
+    "url": "/myfoodapp/static/js/runtime-main.94dbddd2.js"
+  }
+]);
